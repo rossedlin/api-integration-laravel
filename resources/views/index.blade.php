996cecee478deaec4161399418b185e8f36f5183
Ross Edlin <rossedlin@gmail.com>
2022-03-09 13:02:58 +0700

    Change to /contact

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -11,7 +11,7 @@
             Hey, I'm an API Integration.<br />
             I can connect with an API, pull information and show you it.<br/>
             Try me and see what happens. If you like it and want it on your site,
-            <a href="https://www.rossedlin.com/hire/" target="_blank"> contact / hire me here.</a>
+            <a href="https://www.rossedlin.com/contact/" target="_blank"> contact / hire me here.</a>
         </p>
         <p class="g-font-size-16">
             I'm making use of
